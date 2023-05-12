9ef206426235b88ec9a417499e2d71d4a3374d66
DanyonStitt <danyonstitt@gmail.com>
2023-05-12 16:28:40 +1200

    Added cross for closing mobile menu

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const menu = document.getElementById("myLinks");
-const hamburger = document.getElementById("menu-bars")
+const hamburger = document.getElementById("menu-bars");
+const hero = document.querySelector(".hero");
 
 hamburger.addEventListener("click", () => {
     if (menu.style.display == "none") {
-        console.log("naah")
         menu.style.display = "flex";
+        hero.style.display = "none"
+        hamburger.className = "fa-solid fa-xmark"
     }
     else {
-        console.log("yup")
         menu.style.display = "none";
+        hero.style.display = "flex";
+        hamburger.className = "fa-solid fa-bars"
     };
 });
