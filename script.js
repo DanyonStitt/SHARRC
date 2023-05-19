57adee7e5cc61cde6eee1838c7c0ab7ef25d5c19
DanyonStitt <danyonstitt@gmail.com>
2023-05-19 14:16:14 +1200

    Switching drop down indicators automatically

--- script.js
+++ script.js
@@ -14,22 +14,43 @@ function showMenu() {
         menu.style.display = "none";
         hero.style.display = "flex";
         hamburger.className = "fa-solid fa-bars"
+        // If there are open sub-menus, close them and switch the 
+        // drop down indicators back to normal
+        let menuList = document.querySelectorAll(".pop-out-content");
+        let popOutIndicators = document.querySelectorAll(".fa-caret-up");
+        for (i=0; i<popOutIndicators.length; i++) {
+            popOutIndicators[i].className = "fa-solid fa-caret-down"
+        }
+        // For each pop out menu, check the display status
+        for(i=0; i<menuList.length; i++) {
+            let currentDisplay = window.getComputedStyle(
+                menuList[i]).getPropertyValue("display");
+            // If the display is not none, set it to none
+            if (currentDisplay !== "none") {
+                menuList[i].style.display = "none";
+            };
+        };
     };
 };
 
 function showPopOut(menuName) {
-    //  Get the elements involved
+    //  Get name as passed from the HTML file
     let menu = document.querySelector(menuName);
-    let specificMenuItems = menu.className
-    let menuItems = document.getElementById(specificMenuItems)
-    let indicator = document.getElementById("pop-out-indicator");
+    // Find the class name of the tag for selecting the correct menu ID
+    let specificMenuItems = menu.className;
+    // Get the specific menu
+    let menuItems = document.getElementById(specificMenuItems);
+    // Define the name of the indicator ID
+    let indicatorName = "pop-out-indicator-" + specificMenuItems;
+    // Get the specific drop down indicator
+    let indicator = document.getElementById(indicatorName);
 
     //  Get the current display style
     let menuDisplay = window.getComputedStyle(menuItems).getPropertyValue("display");
     
     // If the menu is not displayed, display it
     if (menuDisplay === "none") {
-        // Close any already open sub-menus
+        // Close any already open sub-menus and switch their indicators
         let menuList = document.querySelectorAll(".pop-out-content");
         // For each pop out menu, check the display status
         for(i=0; i<menuList.length; i++) {
@@ -40,8 +61,12 @@ function showPopOut(menuName) {
                 menuList[i].style.display = "none";
             };
         };
+        let popOutIndicators = document.querySelectorAll(".fa-caret-up");
+        for (i=0; i<popOutIndicators.length; i++) {
+            popOutIndicators[i].className = "fa-solid fa-caret-down"
+        }
         menuItems.style.display = "flex";
-        indicator.className = "fa-solid fa-caret-up"
+        indicator.className = "fa-solid fa-caret-up";
     }
     // Otherwise close the list
     else {
